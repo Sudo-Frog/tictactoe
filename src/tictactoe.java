4b0c7ce2126696992834467d99c9622dbf66b1b8
Gabriel <webbegabrielcs@gmail.com>
2025-11-04 15:24:25 -0500

    Attempted to create a red line (rectangle) to strikethrough the winning combo when a winner (if a winner) is made.   -However, due to Java AWT painting order, buttons are heavyweight and therefor cover anything drawn on the Frame in the same place. They are drawn by the system's native. This is not the first issue with using Java AWT.   -Rather than create new Panels and work within those, I think switching this project to Java Swing and using JFrame, JButton, and paintComponent() is best. Swing should be much easier to manage drawing and managing a UI anyway.
    **Beginning research into Java Swing.

--- src/tictactoe.java
+++ src/tictactoe.java
@@ -8,7 +8,8 @@ class Fdemo extends Frame implements ActionListener {
     Label statusLabel;
 
     int turn = 0; // even = O's turn odd = X's turn
-   boolean gameOver = false; //to track if the game has ended
+    boolean gameOver = false; //to track if the game has ended
+    int[] winCombo = null; //the indices of the winning combo; used in winning line draw. Storing it this way allows for scalability
 
     Fdemo() {
         setLayout(null);
@@ -80,16 +81,22 @@ class Fdemo extends Frame implements ActionListener {
         }
     }
 
-    //Reset the game
+    /**
+     * Method to reset the game of TicTacToe for another round
+     */
     private void resetGame() {
         for (Button btn : b) {
             btn.setLabel("");
             btn.setEnabled(true); //re-enable the buttons for a new game
+            btn.setBackground(Color.orange);
         }
         statusLabel.setText("");
         turn = 0;
         gameOver = false; //set to false to allow another game
+        winCombo = null;  //Reset the winning combo
+        repaint();
     }
+
 
     /**
      * Called to check for a winner, when a button is clicked
@@ -101,6 +108,8 @@ class Fdemo extends Frame implements ActionListener {
             {0,4,8}, {2,4,6}           // diagonals
         };
 
+        //Now check each of the indices listed in the possible wins[][] array and check if any
+         //of the 3 combinations of indices have the same label that isn't "" (empty)
         for (int[] combo : wins) {
             String s1 = b[combo[0]].getLabel();
             String s2 = b[combo[1]].getLabel();
@@ -111,6 +120,8 @@ class Fdemo extends Frame implements ActionListener {
                 statusLabel.setText(winner + " wins!");
                 gameOver = true;
                 disableButtons();
+                winCombo = combo;
+                repaint();  //trigger a redraw to show the victory line
                 return;
             }
         }
@@ -118,7 +129,7 @@ class Fdemo extends Frame implements ActionListener {
         // Draw condition
         boolean allFilled = true;
         for (Button btn : b) {
-            if (btn.getLabel().equals("")) {
+            if (btn.getLabel().equals("")){
                 allFilled = false;
                 break;
             }
@@ -129,9 +140,41 @@ class Fdemo extends Frame implements ActionListener {
         }
     }
 
-    private void disableButtons() {
-        for (Button btn : b) btn.removeActionListener(this);
+    /**
+     * Method used to create the victory line across a combo to show the winning 3-square combo
+     *  Override paint() from Frame class
+     */
+    public void paint(Graphics g){
+        super.paint(g);
+
+        if(winCombo != null){
+            g.setColor(Color.red);
+            g.setFont(new Font("", Font.BOLD, 18));
+            g.drawString("Winning line!", 500, 200);
+
+            //Get the coordinates of first and last buttons in the winning combo
+            Rectangle r1 = b[winCombo[0]].getBounds();
+            Rectangle r3 = b[winCombo[2]].getBounds();
+                int x1 = r1.x + r1.width / 2;
+                int y1 = r1.y + r1.height / 2;
+                int x2 = r3.x + r3.width / 2;
+                int y2 = r3.y + r3.height / 2;
+
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setStroke(new BasicStroke(5));
+            g2.drawLine(x1, y1, x2, y2);
+        }
     }
+
+
+    /**
+     * Method to disable the game buttons (X/O) when game is over.
+     */
+    private void disableButtons() {
+        //for (Button btn : b) btn.removeActionListener(this);
+        for (Button btn : b) btn.setEnabled(false);
+    }
+
 
     public static void main(String[] args) {
         new Fdemo();
